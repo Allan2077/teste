3043a0a809556899376cddef4fe17e9e06794703
Allan2077 <allan.cesar@hotmail.com>
2020-07-11 20:48:02 -0300

    Meu commit

---
+++ src/main/java/io/github/allan2077/Main.java
@@ -0,0 +1,25 @@
+package io.github.allan2077;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+class Batata
+{
+    int valor;
+    Batata(int valor){
+        this.valor = valor;
+    }
+}
+
+public class Main {
+
+    public static void main(String args[]){
+        List<Batata> batatats = Arrays.asList(new Batata(1), new Batata(2));
+
+        for(Batata b : batatats){
+            System.out.println(b.valor);
+        }
+    }
+}
+
